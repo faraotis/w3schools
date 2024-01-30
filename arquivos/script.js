daa2caca1a12a79357f348af7aa2746146e3fd46
Faraotis <137304409+faraotis@users.noreply.github.com>
2024-01-29 23:50:07 -0300

    mudei o section do monitor do site

--- arquivos/script.js
+++ arquivos/script.js
@@ -12,21 +12,48 @@ loginHover.addEventListener("mouseout", function() {
 })
 
 //slide
-var radio = document.querySelector('.manual-btn')
-var cont = 1
+var Button1 = document.getElementById('btn1')
+var Button2 = document.getElementById('btn2')
+var Button3 = document.getElementById('btn3')
 
-document.getElementById('radio1').checked = true
+var firstImg = document.querySelector('.primeiro')
 
-setInterval(() => {
-    proximaImg()
-}, 5000)
+// Ainda estou fazendo essa parte...
+// var cont = 0
+// function setaL() {
+//     cont ++
+//     if(cont > 2) {
+//         firstImg.style.marginLeft = '0%'
+//     } else {
+//         cont = 0
+//         firstImg.style.marginLeft = '-200%'
+//     }
+// }
+// function setaR() {
+//     cont ++
+//     if(cont > 2) {
+//         firstImg.style.marginRight = '0%'
+//     } else {
+//         cont = 0
+//         firstImg.style.marginRight = '-200%'
+//     }
+// }
 
-function proximaImg() {
-    cont ++
-
-    if(cont > 3) {
-        cont = 1
-    }
-
-    document.getElementById('radio' + cont).checked = true
+function btn1() {
+    firstImg.style.marginLeft = '0%'
+    Button1.style.backgroundColor = '#595959'
+    Button2.style.backgroundColor = '#aeaeae'
+    Button3.style.backgroundColor = '#aeaeae'
+}
+function btn2() {
+    firstImg.style.marginLeft = '-100%'
+    Button1.style.backgroundColor = '#aeaeae'
+    Button2.style.backgroundColor = '#595959'
+    Button3.style.backgroundColor = '#aeaeae'
+}
+function btn3() {
+    firstImg.style.marginLeft = '-200%'
+    Button1.style.backgroundColor = '#aeaeae'
+    Button2.style.backgroundColor = '#aeaeae'
+    Button3.style.backgroundColor = '#595959'
 }
